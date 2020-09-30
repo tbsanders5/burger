9cb34de7cf846176c6ff0c79da8d3fecf814d69e
Monk <tbsanders5@gmail.com>
2020-09-30 11:21:37 -0700

    created database, server file, installed package

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Double Double", false);
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("BBQ Burger", false);
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Bleu Burger", false);
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Royal With Cheese", false);
