18316c33d790b4a25f996df577ca61f409622435
Monk <tbsanders5@gmail.com>
2020-10-03 16:19:08 -0700

    added lots of functionality and styled the page

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
 const express = require("express");
+
+var PORT = process.env.PORT || 8080;
+
+var app = express();
+
+app.use("/public", express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+  console.log("Server listening on: http://localhost:" + PORT);
+});
