22928926e3b95c677cdffa8a0b1f60045266333a
Monk <tbsanders5@gmail.com>
2020-10-03 18:39:10 -0700

    removed comments to clean up code

--- public/js/burgers.js
+++ public/js/burgers.js
@@ -1,6 +1,5 @@
-// Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function() {
-    // Change devour state on click of .change-devour
+
     $('.change-devour').on('click', function(event) {
       event.preventDefault();
       const id = $(this).data('id');
@@ -10,19 +9,16 @@ $(function() {
         devoured: newDevour,
       };
   
-      // Send the PUT request.
       $.ajax(`/api/burgers/${id}`, {
         type: 'PUT',
         data: newDevouredState,
       }).then(function() {
         console.log('changed devoured to', newDevour);
-        // Reload the page to get the updated list
         location.reload();
       });
     });
   
     $('.create-form').on('submit', function(event) {
-      // Make sure to preventDefault on a submit event.
       event.preventDefault();
   
       const burgerValue = $('#br')
@@ -36,28 +32,23 @@ $(function() {
           devoured: 0,
         };
   
-        // Send the POST request.
         $.ajax('/api/burgers', {
           type: 'POST',
           data: newBurger,
         }).then(function() {
           console.log('created new burger');
-          // Reload the page to get the updated list
           location.reload();
         });
       }
     });
   
-    // Delete selected burger
     $('.delete-burger').on('click', function(event) {
       const id = $(this).data('id');
   
-      // Send the DELETE request.
       $.ajax(`/api/burgers/${id}`, {
         type: 'DELETE',
       }).then(function() {
         console.log('deleted burger', id);
-        // Reload the page to get the updated list
         location.reload();
       });
     });
