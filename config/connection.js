bbfac9ceec0da8533d883123a142555b02187325
Monk <tbsanders5@gmail.com>
2020-10-03 17:34:12 -0700

    more heroku tinkering

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ const mysql = require("mysql");
 
 var connection = mysql.createConnection({
     host: "localhost",
-    port: 8889,
+    port: 3306,
     user: "root",
     password: "root",
     database: "burgers_db"
